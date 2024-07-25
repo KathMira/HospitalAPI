afa1789a9abdcce98024b03dcc74443bf810d5dd
Kathellen Mira <149022813+KathMira@users.noreply.github.com>
2024-07-25 17:16:31 -0300

    Adicionando LaudoDto e LaudoController

---
+++ HospitalAPI/Controllers/LaudoController.cs
@@ -0,0 +1,45 @@
+﻿using HospitalAPI.Banco;
+using HospitalAPI.DTOs.Entrada;
+using HospitalAPI.Modelos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class LaudoController : ControllerBase
+{
+    public HospitalAPIContext _context;
+
+    public LaudoController(HospitalAPIContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Cadastrarlaudo([FromBody] CadastrarLaudoDto cadastrarLaudoDto)
+    {
+        Laudo laudo = new Laudo(cadastrarLaudoDto);
+        _context.Laudos.Add(laudo);
+        await _context.SaveChangesAsync();
+        return Ok("Laudo cadastrado com sucesso!");
+    }
+    [HttpGet]
+    public async Task<IActionResult> VerTodosLaudos()
+    {
+        List<Laudo> vertodoslaudos = await _context.Laudos.ToListAsync();
+        return Ok(vertodoslaudos);
+    }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> VerLaudoPorId([FromRoute] int id)
+    {
+            Laudo? laudo = await _context.Laudos.FirstOrDefaultAsync(x => x.Id == id);
+            if (laudo == null)
+            {
+                return BadRequest("Não achei fio");
+            }
+            return Ok(laudo);
+       
+    }
+}
---
+++ HospitalAPI/DTOs/Entrada/CadastrarLaudoDto.cs
@@ -0,0 +1,14 @@
+﻿namespace HospitalAPI.DTOs.Entrada;
+
+public class CadastrarLaudoDto
+{
+    public int ConsultaId { get; set; }
+    public int MedicoId { get; set; }
+    public int PacienteId { get; set; }
+    public int ExameId { get; set; }
+    public string? CID { get; set; }
+    public DateTime DataLaudo { get; set; }
+    public string NomeLaudo { get; set; } = string.Empty;
+    public string DescricaoLaudo { get; set; } = string.Empty;
+    
+}
